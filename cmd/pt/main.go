1e8e47a3fdecc818ebea1225ad8455a213e4eac5
Michael Fogleman <fogleman@gmail.com>
2015-01-25 15:54:30 -0500

    multiple scenes

--- cmd/pt/main.go
+++ cmd/pt/main.go
@@ -7,28 +7,41 @@ import (
 	"runtime"
 )
 
-func main() {
-	// use all cores
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	// place objects
-	scene := pt.Scene{}
+func scene1() (pt.Scene, pt.Camera) {
+	scene, camera := pt.Scene{}, pt.Camera{}
 	scene.AddShape(&pt.Sphere{pt.Vector{0, 0, 0}, 1, pt.HexColor(0x334D5C)})
 	scene.AddShape(&pt.Sphere{pt.Vector{-2, 0, -2}, 1, pt.HexColor(0x45B29D)})
 	scene.AddShape(&pt.Sphere{pt.Vector{-2, 0, 2}, 1, pt.HexColor(0xEFC94C)})
 	scene.AddShape(&pt.Sphere{pt.Vector{2, 0, -2}, 1, pt.HexColor(0xE27A3F)})
 	scene.AddShape(&pt.Sphere{pt.Vector{2, 0, 2}, 1, pt.HexColor(0xDF5A49)})
 	scene.AddShape(&pt.Cube{pt.Vector{-10, -2, -10}, pt.Vector{10, -1, 10}, pt.Color{1, 1, 1}})
-
-	// place lights
 	scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, -1}, 0.25, pt.Color{0.8, 0.8, 0.8}})
-
-	// place camera
-	camera := pt.Camera{}
 	camera.LookAt(pt.Vector{0, 6, -8}, pt.Vector{0, 0, -1}, pt.Vector{0, 1, 0}, 45)
+	return scene, camera
+}
+
+func scene2() (pt.Scene, pt.Camera) {
+	scene, camera := pt.Scene{}, pt.Camera{}
+	scene.AddShape(&pt.Cube{pt.Vector{-10, -1, -10}, pt.Vector{10, 0, 10}, pt.Color{1, 1, 1}})
+	for x := -2; x <= 2; x++ {
+		for z := -2; z <= 2; z++ {
+			scene.AddShape(&pt.Sphere{pt.Vector{float64(x) * 2, 0.8, float64(z) * 2}, 0.8, pt.HexColor(0x334D5C)})
+		}
+	}
+	scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, -1}, 0.25, pt.Color{0.8, 0.8, 0.8}})
+	camera.LookAt(pt.Vector{0, 6, -2}, pt.Vector{0, 0, -1}, pt.Vector{0, 1, 0}, 55)
+	return scene, camera
+}
+
+func main() {
+	// use all cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// pick a scene
+	scene, camera := scene1()
 
 	// render image
-	image := pt.Render(&scene, &camera, 2560, 1440, 16, 16, 4)
+	image := pt.Render(&scene, &camera, 2560/4, 1440/4, 16, 16, 4)
 
 	// save as png
 	file, err := os.Create("out.png")
