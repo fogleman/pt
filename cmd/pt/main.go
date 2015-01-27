4181e37835122466418c653ed01573020bb49213
Michael Fogleman <fogleman@gmail.com>
2015-01-26 23:50:53 -0500

    scene3

--- cmd/pt/main.go
+++ cmd/pt/main.go
@@ -16,10 +16,11 @@ func scene1() (pt.Scene, pt.Camera) {
 	scene.AddShape(&pt.Sphere{pt.Vector{2, 0, -2}, 1, pt.HexColor(0xE27A3F), pt.Material{}})
 	scene.AddShape(&pt.Sphere{pt.Vector{2, 0, 2}, 1, pt.HexColor(0xDF5A49), pt.Material{}})
 	scene.AddShape(&pt.Cube{pt.Vector{-10, -2, -10}, pt.Vector{10, -1, 10}, pt.Color{1, 1, 1}, pt.Material{}})
-	scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, -1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
-	scene.AddLight(&pt.Sphere{pt.Vector{1, 4, -1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
-	scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, 1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
-	scene.AddLight(&pt.Sphere{pt.Vector{1, 4, 1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	// scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, -1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	// scene.AddLight(&pt.Sphere{pt.Vector{1, 4, -1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	// scene.AddLight(&pt.Sphere{pt.Vector{-1, 4, 1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	// scene.AddLight(&pt.Sphere{pt.Vector{1, 4, 1}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	scene.AddLight(&pt.Cube{pt.Vector{-5, 8, -5}, pt.Vector{5, 9, 5}, pt.Color{1, 1, 1}, pt.Material{}})
 	camera.LookAt(pt.Vector{0, 6, -8}, pt.Vector{0, 0, -1}, pt.Vector{0, 1, 0}, 45)
 	return scene, camera
 }
@@ -50,6 +51,20 @@ func scene2() (pt.Scene, pt.Camera) {
 	return scene, camera
 }
 
+func scene3() (pt.Scene, pt.Camera) {
+	scene, camera := pt.Scene{}, pt.Camera{}
+	for x := 0; x < 10; x++ {
+		for z := 0; z < 10; z++ {
+			scene.AddShape(&pt.Sphere{pt.Vector{float64(x) - 4.5, 0, float64(z) - 4.5}, 0.45, pt.HexColor(0xEFC94C), pt.Material{}})
+		}
+	}
+	scene.AddShape(&pt.Cube{pt.Vector{-100, -2, -100}, pt.Vector{100, 0, 100}, pt.Color{1, 1, 1}, pt.Material{}})
+	// scene.AddLight(&pt.Cube{pt.Vector{-5, 8, -5}, pt.Vector{5, 9, 5}, pt.Color{1, 1, 1}, pt.Material{}})
+	scene.AddLight(&pt.Sphere{pt.Vector{0, 2, 0}, 0.25, pt.Color{1, 1, 1}, pt.Material{}})
+	camera.LookAt(pt.Vector{0, 5, -8}, pt.Vector{0, 0, -2}, pt.Vector{0, 1, 0}, 45)
+	return scene, camera
+}
+
 func save(path string, im image.Image) {
 	file, err := os.Create(path)
 	if err != nil {
@@ -64,7 +79,7 @@ func save(path string, im image.Image) {
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	scene, camera := scene1()
+	scene, camera := scene3()
 	im := pt.Render(&scene, &camera, 2560/4, 1440/4, 16, 16, 4)
 	save("out.png", im)
 }
