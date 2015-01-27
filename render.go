41d92aba87944f8aaf2ae1a836328c336b086088
Michael Fogleman <fogleman@gmail.com>
2015-01-27 15:27:43 -0500

    showProgress helper

--- render.go
+++ render.go
@@ -10,6 +10,23 @@ import (
 	"time"
 )
 
+func showProgress(start time.Time, i, h int) {
+	pct := int(100 * float64(i+1) / float64(h))
+	elapsed := time.Since(start)
+	hr := int(elapsed.Hours())
+	min := int(elapsed.Minutes()) % 60
+	sec := int(elapsed.Seconds()) % 60
+	fmt.Printf("\r%4d / %d (%3d%%) [", i+1, h, pct)
+	for p := 0; p < 100; p += 2 {
+		if pct > p {
+			fmt.Printf("=")
+		} else {
+			fmt.Printf(" ")
+		}
+	}
+	fmt.Printf("] %d:%02d:%02d", hr, min, sec)
+}
+
 func Render(scene *Scene, camera *Camera, w, h, cameraSamples, hitSamples, depth int) image.Image {
 	image := image.NewNRGBA(image.Rect(0, 0, w, h))
 	ncpu := runtime.GOMAXPROCS(0)
@@ -42,22 +59,10 @@ func Render(scene *Scene, camera *Camera, w, h, cameraSamples, hitSamples, depth
 			}
 		}(i)
 	}
+	showProgress(start, -1, h)
 	for i := 0; i < h; i++ {
 		<-ch
-		pct := int(100 * float64(i) / float64(h-1))
-		elapsed := time.Since(start)
-		hr := int(elapsed.Hours())
-		min := int(elapsed.Minutes()) % 60
-		sec := int(elapsed.Seconds()) % 60
-		fmt.Printf("\r%4d / %d (%3d%%) [", i+1, h, pct)
-		for p := 0; p < 100; p += 2 {
-			if pct >= p {
-				fmt.Printf("=")
-			} else {
-				fmt.Printf(" ")
-			}
-		}
-		fmt.Printf("] %d:%02d:%02d", hr, min, sec)
+		showProgress(start, i, h)
 	}
 	fmt.Println()
 	return image
