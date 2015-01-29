306132470c52c16a75a846caf22463b0a6b3ad71
Michael Fogleman <fogleman@gmail.com>
2015-01-28 22:25:43 -0500

    plane shape

---
+++ plane.go
@@ -0,0 +1,44 @@
+package pt
+
+import (
+	"math"
+	"math/rand"
+)
+
+type Plane struct {
+	center, normal Vector
+	color          Color
+	material       Material
+}
+
+func NewPlane(center, normal Vector, color Color, material Material) Shape {
+	return &Plane{center, normal, color, material}
+}
+
+func (p *Plane) Intersect(r Ray) float64 {
+	k := r.Direction.Dot(p.normal)
+	if math.Abs(k) < EPS {
+		return INF
+	}
+	t := p.center.Sub(r.Origin).Dot(p.normal) / k
+	if t < EPS {
+		return INF
+	}
+	return t
+}
+
+func (p *Plane) Color(v Vector) Color {
+	return p.color
+}
+
+func (p *Plane) Material(v Vector) Material {
+	return p.material
+}
+
+func (p *Plane) Normal(v Vector) Vector {
+	return p.normal
+}
+
+func (p *Plane) RandomPoint(rnd *rand.Rand) Vector {
+	return Vector{} // TODO: fix
+}
