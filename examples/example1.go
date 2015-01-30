3d1a0d19d2cb7e1f3404c8e0e0cd83c77415fd0e
Michael Fogleman <fogleman@gmail.com>
2015-01-30 14:45:52 -0500

    examples folder

---
+++ examples/example1.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/fogleman/pt"
+
+func main() {
+	scene := pt.Scene{}
+	scene.AddShape(pt.NewSphere(pt.Vector{1.5, 1, 0}, 1, pt.HexColor(0x334D5C), pt.RefractiveMaterial(2), nil))
+	scene.AddShape(pt.NewSphere(pt.Vector{-1, 1, 2}, 1, pt.HexColor(0xEFC94C), pt.RefractiveMaterial(2), nil))
+	scene.AddShape(pt.NewPlane(pt.Vector{0, 0, 0}, pt.Vector{0, 1, 0}, pt.Color{1, 1, 1}, pt.RefractiveMaterial(1.33)))
+	scene.AddLight(pt.NewSphere(pt.Vector{-1, 3, -1}, 0.5, pt.Color{1, 1, 1}, pt.DiffuseMaterial(), nil))
+	camera := pt.LookAt(pt.Vector{0, 2, -5}, pt.Vector{0, 0, 3}, pt.Vector{0, 1, 0}, 45)
+	im := pt.Render(&scene, &camera, 2560/4, 1440/4, 4, 16, 8)
+	if err := pt.Save("out.png", im); err != nil {
+		panic(err)
+	}
+}
