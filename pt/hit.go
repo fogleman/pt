939d78545e91f34e14f39ad6bd5384ebdb32b433
Michael Fogleman <fogleman@gmail.com>
2015-02-05 10:47:38 -0500

    Info -> HitInfo

--- pt/hit.go
+++ pt/hit.go
@@ -5,27 +5,27 @@ type Hit struct {
 	T     float64
 }
 
-var NoHit = Hit{T: INF}
-
-func (hit *Hit) Ok() bool {
-	return hit.T < INF
-}
-
-func (hit *Hit) Info(r Ray) Info {
-	shape := hit.Shape
-	position := r.Position(hit.T)
-	normal := shape.Normal(position)
-	ray := Ray{position, normal}
-	color := shape.Color(position)
-	material := shape.Material(position)
-	return Info{shape, position, normal, ray, color, material}
-}
-
-type Info struct {
+type HitInfo struct {
 	Shape    Shape
 	Position Vector
 	Normal   Vector
 	Ray      Ray
 	Color    Color
 	Material Material
+}
+
+var NoHit = Hit{T: INF}
+
+func (hit *Hit) Ok() bool {
+	return hit.T < INF
+}
+
+func (hit *Hit) Info(r Ray) HitInfo {
+	shape := hit.Shape
+	position := r.Position(hit.T)
+	normal := shape.Normal(position)
+	ray := Ray{position, normal}
+	color := shape.Color(position)
+	material := shape.Material(position)
+	return HitInfo{shape, position, normal, ray, color, material}
 }
