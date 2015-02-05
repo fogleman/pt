285a56321cbe462681d5a46727d390fac722b780
Michael Fogleman <fogleman@gmail.com>
2015-02-05 16:56:24 -0500

    smooth gopher normals

--- examples/gopher.go
+++ examples/gopher.go
@@ -8,9 +8,10 @@ func main() {
 	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{30, 0.376662, 30}, pt.HexColor(0xFCFAE1), pt.Material{1.5, 0, 0}))
 	mesh := pt.NewMesh(pt.HexColor(0x5BCFDE), pt.Material{1.5, 0, 0})
 	mesh.LoadOBJ("examples/gopher.obj")
+	mesh.SmoothNormals()
 	scene.AddShape(mesh)
 	camera := pt.LookAt(pt.Vector{8, 3, 0.5}, pt.Vector{-1, 2.5, 0.5}, pt.Vector{0, 1, 0}, 45)
-	im := pt.Render(&scene, &camera, 2560/4, 1440/4, 16, 64, 4)
+	im := pt.Render(&scene, &camera, 2560/2, 1440/2, 1, 16, 4)
 	if err := pt.SavePNG("out.png", im); err != nil {
 		panic(err)
 	}
