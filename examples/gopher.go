44067f0acb6a9063b26e9974de75c8f5ed3aff84
Michael Fogleman <fogleman@gmail.com>
2015-02-05 22:36:13 -0500

    iterative render for the gopher

--- examples/gopher.go
+++ examples/gopher.go
@@ -8,11 +8,10 @@ func main() {
 	scene.AddLight(pt.NewSphere(pt.Vector{2, 8, 0.5}, 2, pt.DiffuseMaterial(pt.Color{1, 1, 1}), nil))
 	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{-8, 10, 30}, wall))
 	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{30, 0.376662, 30}, wall))
-	material := pt.Material{pt.Color{}, 2, pt.Radians(20), 0}
+	material := pt.Material{pt.Color{}, 1.5, pt.Radians(30), 0}
 	mesh, _ := pt.LoadOBJ("examples/gopher.obj", material)
 	mesh.SmoothNormals()
 	scene.AddShape(mesh)
 	camera := pt.LookAt(pt.Vector{8, 3, 0.5}, pt.Vector{-1, 2.5, 0.5}, pt.Vector{0, 1, 0}, 45)
-	im := pt.Render(&scene, &camera, 2560/2, 1440/2, 16, 64, 4)
-	pt.SavePNG("out.png", im)
+	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560, 1440, 0, 16, 4)
 }
