2c8d9ab2f8bb78a9d5e56fdc5650c3d81893d4e9
Michael Fogleman <fogleman@gmail.com>
2015-02-06 14:33:13 -0500

    tweaked gopher scene

--- examples/gopher.go
+++ examples/gopher.go
@@ -4,14 +4,16 @@ import "github.com/fogleman/pt/pt"
 
 func main() {
 	scene := pt.Scene{}
-	wall := pt.Material{pt.HexColor(0xFCFAE1), 2, 0, 0}
-	scene.AddLight(pt.NewSphere(pt.Vector{2, 8, 0.5}, 2, pt.DiffuseMaterial(pt.Color{1, 1, 1}), nil))
-	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{-8, 10, 30}, wall))
-	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{30, 0.376662, 30}, wall))
-	material := pt.Material{pt.Color{}, 1.5, pt.Radians(30), 0}
-	mesh, _ := pt.LoadOBJ("examples/gopher.obj", material)
+	gopher := pt.Material{pt.Color{}, 2, 0, 0}
+	wall := pt.Material{pt.HexColor(0xFCFAE1), 2, pt.Radians(30), 0}
+	floor := pt.Material{pt.HexColor(0xFCFAE1), 2, pt.Radians(30), 0}
+	scene.AddLight(pt.NewSphere(pt.Vector{10, 10, 10}, 2, pt.DiffuseMaterial(pt.Color{1, 1, 1}), nil))
+	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{-5, 10, 30}, wall))
+	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{30, 0, 30}, floor))
+	mesh, _ := pt.LoadOBJ("examples/gopher.obj", gopher)
 	mesh.SmoothNormals()
+	mesh.MoveTo(pt.Vector{}, pt.Vector{0.5, 0, 0.5})
 	scene.AddShape(mesh)
-	camera := pt.LookAt(pt.Vector{8, 3, 0.5}, pt.Vector{-1, 2.5, 0.5}, pt.Vector{0, 1, 0}, 45)
-	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560, 1440, 0, 16, 4)
+	camera := pt.LookAt(pt.Vector{10, 3, 0}, pt.Vector{0, 2.5, 0}, pt.Vector{0, 1, 0}, 45)
+	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/4, 1440/4, 0, 16, 4)
 }
