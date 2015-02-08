78b224a8ebcb11e55f5436ca8cff743d4f6df855
Michael Fogleman <fogleman@gmail.com>
2015-02-07 22:38:09 -0500

    multiple gophers

--- examples/gopher.go
+++ examples/gopher.go
@@ -1,27 +1,21 @@
 package main
 
-import "fmt"
 import "github.com/fogleman/pt/pt"
 
-func render(angle int) {
+func main() {
 	scene := pt.Scene{}
 	wall := pt.Material{pt.HexColor(0xFCFAE1), 2, 0, 0}
-	scene.AddLight(pt.NewSphere(pt.Vector{2, 8, 0.5}, 2, pt.DiffuseMaterial(pt.Color{1, 1, 1}), nil))
-	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{-8, 10, 30}, wall))
+	scene.AddLight(pt.NewSphere(pt.Vector{2, 30, 0.5}, 2, pt.DiffuseMaterial(pt.Color{1, 1, 1}), nil))
+	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{-8, 30, 30}, wall))
 	scene.AddShape(pt.NewCube(pt.Vector{-30, -1, -30}, pt.Vector{30, 0, 30}, wall))
 	material := pt.Material{pt.Color{}, 1.5, pt.Radians(30), 0}
 	mesh, _ := pt.LoadOBJ("examples/gopher.obj", material)
 	mesh.SmoothNormals()
 	mesh.MoveTo(pt.Vector{}, pt.Vector{0.5, 0, 0.5})
-	m := pt.Rotate(pt.Vector{0, 1, 0}, pt.Radians(float64(angle)))
-	scene.AddShape(pt.NewTransformedShape(mesh, m))
-	camera := pt.LookAt(pt.Vector{10, 3, 0}, pt.Vector{-1, 2, 0}, pt.Vector{0, 1, 0}, 45)
-	path := fmt.Sprintf("out%03d.png", angle)
-	pt.IterativeRender(path, 1, &scene, &camera, 256, 256, 4, 16, 4)
-}
-
-func main() {
-	for i := 0; i < 360; i += 5 {
-		render(i)
+	for i := -2; i <= 2; i++ {
+		m := pt.Translate(pt.Vector{0, 0, float64(i) * 5})
+		scene.AddShape(pt.NewTransformedShape(mesh, m))
 	}
+	camera := pt.LookAt(pt.Vector{20, 4, 0}, pt.Vector{-1, 2, 0}, pt.Vector{0, 1, 0}, 45)
+	pt.IterativeRender("out.png", 1000, &scene, &camera, 2560/4, 1440/4, 0, 16, 4)
 }
