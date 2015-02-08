13912cd1c5f1f1d5c6a624e200c3d3a6898797af
Michael Fogleman <fogleman@gmail.com>
2015-02-07 22:35:18 -0500

    TransformedShape.Box

--- pt/shape.go
+++ pt/shape.go
@@ -1,6 +1,7 @@
 package pt
 
 import (
+	"math"
 	"math/rand"
 )
 
@@ -24,10 +25,31 @@ func NewTransformedShape(s Shape, m Matrix) Shape {
 	return &TransformedShape{s, m, m.Inverse()}
 }
 
-// TODO: transformed Box()
+func (s *TransformedShape) Box() Box {
+	// http://dev.theomader.com/transform-bounding-boxes/
+	b := s.Shape.Box()
+	minx, maxx := b.Min.X, b.Max.X
+	miny, maxy := b.Min.Y, b.Max.Y
+	minz, maxz := b.Min.Z, b.Max.Z
+	m := &s.matrix
+	xa := m.x00 * minx + m.x10 * minx + m.x20 * minx + m.x30 * minx
+	xb := m.x00 * maxx + m.x10 * maxx + m.x20 * maxx + m.x30 * maxx
+	ya := m.x01 * miny + m.x11 * miny + m.x21 * miny + m.x31 * miny
+	yb := m.x01 * maxy + m.x11 * maxy + m.x21 * maxy + m.x31 * maxy
+	za := m.x02 * minz + m.x12 * minz + m.x22 * minz + m.x32 * minz
+	zb := m.x02 * maxz + m.x12 * maxz + m.x22 * maxz + m.x32 * maxz
+	minx, maxx = math.Min(xa, xb), math.Max(xa, xb)
+	miny, maxy = math.Min(ya, yb), math.Max(ya, yb)
+	minz, maxz = math.Min(za, zb), math.Max(za, zb)
+	min := Vector{minx + m.x03, miny + m.x13, minz + m.x23}
+	max := Vector{maxx + m.x03, maxy + m.x13, maxz + m.x23}
+	return Box{min, max}
+}
 
 func (s *TransformedShape) Intersect(r Ray) Hit {
 	hit := s.Shape.Intersect(s.inverse.MulRay(r))
+	// if s.Shape is a Mesh, the hit.Shape will be a Triangle in the Mesh
+	// we need to transform this Triangle, not the Mesh itself
 	shape := &TransformedShape{hit.Shape, s.matrix, s.inverse}
 	return Hit{shape, hit.T}
 }
