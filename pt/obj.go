5f313896adab2e4cc4d52f3fc7e6f11675fc938b
Michael Fogleman <fogleman@gmail.com>
2015-02-11 17:03:32 -0500

    support negative indexes in obj files

--- pt/obj.go
+++ pt/obj.go
@@ -4,8 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func parseIndex(value string, length int) int {
+	parsed, _ := strconv.ParseInt(value, 0, 0)
+	n := int(parsed)
+	if n < 0 {
+		n += length
+	}
+	return n
+}
 
 func LoadOBJ(path string, parent Material) (*Mesh, error) {
 	fmt.Printf("Loading OBJ: %s\n", path)
@@ -53,29 +63,26 @@ func LoadOBJ(path string, parent Material) (*Mesh, error) {
 			v := Vector{f[0], f[1], f[2]}
 			vns = append(vns, v)
 		case "f":
-			var fvs, fvts, fvns []string
+			var fvs, fvts, fvns []int
 			for _, arg := range args {
 				vertex := strings.Split(arg+"//", "/")
-				fvs = append(fvs, vertex[0])
-				fvts = append(fvts, vertex[1])
-				fvns = append(fvns, vertex[2])
+				fvs = append(fvs, parseIndex(vertex[0], len(vs)))
+				fvts = append(fvts, parseIndex(vertex[1], len(vts)))
+				fvns = append(fvns, parseIndex(vertex[2], len(vns)))
 			}
-			ivs := ParseInts(fvs)
-			ivts := ParseInts(fvts)
-			ivns := ParseInts(fvns)
-			for i := 1; i < len(ivs)-1; i++ {
+			for i := 1; i < len(fvs)-1; i++ {
 				i1, i2, i3 := 0, i, i+1
 				t := Triangle{}
 				t.material = material
-				t.v1 = vs[ivs[i1]]
-				t.v2 = vs[ivs[i2]]
-				t.v3 = vs[ivs[i3]]
-				t.t1 = vts[ivts[i1]]
-				t.t2 = vts[ivts[i2]]
-				t.t3 = vts[ivts[i3]]
-				t.n1 = vns[ivns[i1]]
-				t.n2 = vns[ivns[i2]]
-				t.n3 = vns[ivns[i3]]
+				t.v1 = vs[fvs[i1]]
+				t.v2 = vs[fvs[i2]]
+				t.v3 = vs[fvs[i3]]
+				t.t1 = vts[fvts[i1]]
+				t.t2 = vts[fvts[i2]]
+				t.t3 = vts[fvts[i3]]
+				t.n1 = vns[fvns[i1]]
+				t.n2 = vns[fvns[i2]]
+				t.n3 = vns[fvns[i3]]
 				t.UpdateBox()
 				t.FixNormals()
 				triangles = append(triangles, &t)
