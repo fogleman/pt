d5d83f73503c5d62b139d3e174780ffe5bbd0e10
Michael Fogleman <fogleman@gmail.com>
2015-02-10 23:09:17 -0500

    updated example

--- examples/sponza.go
+++ examples/sponza.go
@@ -6,12 +6,11 @@ import "github.com/fogleman/pt/pt"
 
 func main() {
 	scene := pt.Scene{}
-	material := pt.DiffuseMaterial(pt.HexColor(0xFCFAE1))
+	material := pt.GlossyMaterial(pt.HexColor(0xFCFAE1), 1.5, pt.Radians(20))
 	mesh, _ := pt.LoadOBJ("examples/dabrovic-sponza/sponza.obj", material)
 	mesh.MoveTo(pt.Vector{}, pt.Vector{0.5, 0, 0.5})
-	// fmt.Println(mesh.Box())
 	scene.Add(mesh)
 	scene.Add(pt.NewSphere(pt.Vector{0, 20, 0}, 3, pt.LightMaterial(pt.Color{1, 1, 1}, 1, pt.NoAttenuation)))
 	camera := pt.LookAt(pt.Vector{-10, 2, 0}, pt.Vector{0, 4, 0}, pt.Vector{0, 1, 0}, 45)
-	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/2, 1440/2, 0, 1, 4)
+	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/2, 1440/2, 0, 16, 3)
 }
