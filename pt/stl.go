82125dcc0409383ee42625170603e89f6181262c
Michael Fogleman <fogleman@gmail.com>
2015-02-13 16:55:47 -0500

    support ASCII STL files with LoadSTL()

--- pt/stl.go
+++ pt/stl.go
@@ -1,9 +1,11 @@
 package pt
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type STLHeader struct {
@@ -17,8 +19,7 @@ type STLTriangle struct {
 }
 
 func LoadBinarySTL(path string, material Material) (*Mesh, error) {
-	fmt.Printf("Loading %s... ", path)
-	defer fmt.Println("OK")
+	fmt.Printf("Loading Binary STL: %s\n", path)
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -49,4 +50,36 @@ func LoadBinarySTL(path string, material Material) (*Mesh, error) {
 		triangles[i] = &t
 	}
 	return NewMesh(triangles), nil
+}
+
+func LoadSTL(path string, material Material) (*Mesh, error) {
+	fmt.Printf("Loading ASCII STL: %s\n", path)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var vertexes []Vector
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) == 4 && fields[0] == "vertex" {
+			f := ParseFloats(fields[1:])
+			v := Vector{f[0], f[1], f[2]}
+			vertexes = append(vertexes, v)
+		}
+	}
+	var triangles []*Triangle
+	for i := 0; i < len(vertexes); i += 3 {
+		t := Triangle{}
+		t.material = &material
+		t.v1 = vertexes[i+0]
+		t.v2 = vertexes[i+1]
+		t.v3 = vertexes[i+2]
+		t.UpdateBox()
+		t.FixNormals()
+		triangles = append(triangles, &t)
+	}
+	return NewMesh(triangles), scanner.Err()
 }
