ebc743b56156bc4fff2ed7b11d5ad62e22ba23ac
Michael Fogleman <fogleman@gmail.com>
2015-02-18 15:31:52 -0500

    updated cylinder for poster

--- examples/cylinder.go
+++ examples/cylinder.go
@@ -19,15 +19,15 @@ func createMesh(material pt.Material) pt.Shape {
 func main() {
 	scene := pt.Scene{}
 	meshes := []pt.Shape{
-		createMesh(pt.GlossyMaterial(pt.HexColor(0x730046), 1.6, pt.Radians(30))),
-		createMesh(pt.GlossyMaterial(pt.HexColor(0xBFBB11), 1.6, pt.Radians(30))),
-		createMesh(pt.GlossyMaterial(pt.HexColor(0xFFC200), 1.6, pt.Radians(30))),
-		createMesh(pt.GlossyMaterial(pt.HexColor(0xE88801), 1.6, pt.Radians(30))),
-		createMesh(pt.GlossyMaterial(pt.HexColor(0xC93C00), 1.6, pt.Radians(30))),
+		createMesh(pt.GlossyMaterial(pt.HexColor(0x730046), 1.6, pt.Radians(45))),
+		createMesh(pt.GlossyMaterial(pt.HexColor(0xBFBB11), 1.6, pt.Radians(45))),
+		createMesh(pt.GlossyMaterial(pt.HexColor(0xFFC200), 1.6, pt.Radians(45))),
+		createMesh(pt.GlossyMaterial(pt.HexColor(0xE88801), 1.6, pt.Radians(45))),
+		createMesh(pt.GlossyMaterial(pt.HexColor(0xC93C00), 1.6, pt.Radians(45))),
 	}
-	for x := -6; x <= 6; x++ {
+	for x := -6; x <= 3; x++ {
 		mesh := meshes[(x+6)%len(meshes)]
-		for y := -6; y <= 6; y++ {
+		for y := -5; y <= 4; y++ {
 			fx := float64(x) / 2
 			fy := float64(y)
 			fz := float64(x) / 2
@@ -35,6 +35,6 @@ func main() {
 		}
 	}
 	scene.Add(pt.NewSphere(pt.Vector{1, 0, 10}, 3, pt.LightMaterial(pt.Color{1, 1, 1}, 1, pt.NoAttenuation)))
-	camera := pt.LookAt(pt.Vector{-5, 0, 5}, pt.Vector{1.2, 0, 0}, pt.Vector{0, 0, 1}, 45)
-	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/2, 1440/2, -1, 16, 3)
+	camera := pt.LookAt(pt.Vector{-5, 0, 5}, pt.Vector{1, 0, 0}, pt.Vector{0, 0, 1}, 45)
+	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 5475, 3675, -1, 16, 3)
 }
