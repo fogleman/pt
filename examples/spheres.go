15d56804ca7bc08b4d58b9a6e2dc34fcec03df4a
Michael Fogleman <fogleman@gmail.com>
2016-02-02 22:04:29 -0500

    new example

---
+++ examples/spheres.go
@@ -0,0 +1,55 @@
+package main
+
+import . "github.com/fogleman/pt/pt"
+
+var materials = []Material{
+	GlossyMaterial(HexColor(0x730046), 1.4, Radians(30)),
+	GlossyMaterial(HexColor(0xBFBB11), 1.4, Radians(30)),
+	GlossyMaterial(HexColor(0xFFC200), 1.4, Radians(30)),
+	GlossyMaterial(HexColor(0xE88801), 1.4, Radians(30)),
+	GlossyMaterial(HexColor(0xC93C00), 1.4, Radians(30)),
+}
+
+func sphere(scene *Scene, previous, center Vector, radius float64, depth int) {
+	if depth <= 0 {
+		return
+	}
+	material := materials[(depth+1)%len(materials)]
+	scene.Add(NewSphere(center, radius, material))
+	r2 := radius / 2.5
+	offset := radius + r2
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			for dz := -1; dz <= 1; dz++ {
+				n := 0
+				if dx != 0 {
+					n++
+				}
+				if dy != 0 {
+					n++
+				}
+				if dz != 0 {
+					n++
+				}
+				if n != 1 {
+					continue
+				}
+				d := Vector{float64(dx), float64(dy), float64(dz)}
+				if d == previous.MulScalar(-1) {
+					continue
+				}
+				c2 := center.Add(d.MulScalar(offset))
+				sphere(scene, d, c2, r2, depth-1)
+			}
+		}
+	}
+}
+
+func main() {
+	scene := Scene{}
+	scene.SetColor(HexColor(0xFFFFFF))
+	sphere(&scene, Vector{}, Vector{}, 1, 7)
+	// scene.Add(NewSphere(Vector{10, 10, 10}, 0.5, LightMaterial(Color{1, 1, 1}, 1, NoAttenuation)))
+	camera := LookAt(Vector{3, 1.75, 1}, Vector{0.75, 0.5, 0}, Vector{0, 0, 1}, 30)
+	IterativeRender("out%03d.png", 10000, &scene, &camera, 2560, 1440, -1, 4, 4)
+}
