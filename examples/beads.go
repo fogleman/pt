be0c5ca4168b4870d11f7f54979a9abda2708072
Michael Fogleman <fogleman@gmail.com>
2016-03-05 22:34:25 -0500

    animate

--- examples/beads.go
+++ examples/beads.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 
 	"github.com/fogleman/pt/pt"
 )
 
-func main() {
+func frame(path string, t float64) {
 	materials := []pt.Material{
-		pt.GlossyMaterial(pt.HexColor(0xFFFFFF), 1e4, 0),
-		pt.TransparentMaterial(pt.HexColor(0xFFFFFF), 1.33, 0, 0),
+		// pt.GlossyMaterial(pt.HexColor(0xFFFFFF), 1e4, 0),
+		// pt.TransparentMaterial(pt.HexColor(0xFFFFFF), 1.33, 0, 0),
 		pt.GlossyMaterial(pt.HexColor(0x167F39), 1.3, pt.Radians(20)),
 		pt.GlossyMaterial(pt.HexColor(0x45BF55), 1.3, pt.Radians(20)),
 		pt.GlossyMaterial(pt.HexColor(0x96ED89), 1.3, pt.Radians(20)),
@@ -27,17 +28,35 @@ func main() {
 		ys := LowPassNoise(n, 0.25, 4)
 		zs := LowPassNoise(n, 0.25, 4)
 		position := pt.Vector{}
+		positions := make([]pt.Vector, n)
 		for i := 0; i < n; i++ {
-			sphere := pt.NewSphere(position, 0.1, material)
-			scene.Add(sphere)
+			positions[i] = position
 			v := pt.Vector{xs[i], ys[i], zs[i]}.Normalize().MulScalar(0.1)
 			position = position.Add(v)
+		}
+		for i := 0; i < n-1; i++ {
+			a := positions[i]
+			b := positions[i+1]
+			p := a.Add(b.Sub(a).MulScalar(t))
+			sphere := pt.NewSphere(p, 0.1, material)
+			scene.Add(sphere)
 		}
 	}
 	scene.Add(pt.NewSphere(pt.Vector{4, 4, 20}, 2, pt.LightMaterial(pt.HexColor(0xFFFFFF), 1, pt.NoAttenuation)))
 	fovy := 40.0
 	camera := pt.LookAt(eye, center, up, fovy)
-	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560, 1440, -1, 4, 4)
+	// pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560, 1440, -1, 4, 4)
+	im := pt.Render(&scene, &camera, 1920, 1080, 16, 4, 4)
+	pt.SavePNG(path, im)
+}
+
+func main() {
+	for i := 0; i < 30; i++ {
+		t := float64(i) / 30
+		path := fmt.Sprintf("out%03d.png", i)
+		fmt.Println(path)
+		frame(path, t)
+	}
 }
 
 func Normalize(values []float64, a, b float64) []float64 {
