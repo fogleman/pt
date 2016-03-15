ca8e089001ebecba39932d5a4963cf02f235b047
Michael Fogleman <fogleman@gmail.com>
2016-03-14 23:02:47 -0400

    ellipsoids

--- examples/ellipsoid.go
+++ examples/ellipsoid.go
@@ -4,18 +4,19 @@ import "github.com/fogleman/pt/pt"
 
 func main() {
 	scene := pt.Scene{}
-	wall := pt.SpecularMaterial(pt.HexColor(0xFCFAE1), 2)
+	wall := pt.GlossyMaterial(pt.HexColor(0xFCFAE1), 1.333, pt.Radians(30))
 	scene.Add(pt.NewSphere(pt.Vector{10, 10, 10}, 2, pt.LightMaterial(pt.Color{1, 1, 1}, 1, pt.NoAttenuation)))
-	scene.Add(pt.NewCube(pt.Vector{-100, -100, -100}, pt.Vector{-2, 100, 100}, wall))
+	scene.Add(pt.NewCube(pt.Vector{-100, -100, -100}, pt.Vector{-6, 100, 100}, wall))
 	scene.Add(pt.NewCube(pt.Vector{-100, -100, -100}, pt.Vector{100, -1, 100}, wall))
-	material := pt.GlossyMaterial(pt.HexColor(0x167F39), 1.5, pt.Radians(30))
+	material := pt.GlossyMaterial(pt.HexColor(0x167F39), 1.333, pt.Radians(30))
 	sphere := pt.NewSphere(pt.Vector{}, 1, material)
-	m := pt.Scale(pt.Vector{1, 1, 2})
-	m = m.Rotate(pt.Vector{0, 1, 0}, pt.Radians(15))
-	m = m.Translate(pt.Vector{0, 0, -2})
-	shape := pt.NewTransformedShape(sphere, m)
-	scene.Add(shape)
-	scene.Add(pt.NewSphere(pt.Vector{0, 0, 2}, 1, material))
-	camera := pt.LookAt(pt.Vector{8, 3, 0}, pt.Vector{}, pt.Vector{0, 1, 0}, 45)
-	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/4, 1440/4, -1, 16, 4)
+	for i := 0; i < 180; i += 30 {
+		m := pt.Identity()
+		m = m.Scale(pt.Vector{0.3, 1, 5})
+		m = m.Rotate(pt.Vector{0, 1, 0}, pt.Radians(float64(i)))
+		shape := pt.NewTransformedShape(sphere, m)
+		scene.Add(shape)
+	}
+	camera := pt.LookAt(pt.Vector{8, 8, 0}, pt.Vector{1, 0, 0}, pt.Vector{0, 1, 0}, 45)
+	pt.IterativeRender("out%03d.png", 1000, &scene, &camera, 2560/2, 1440/2, -1, 4, 4)
 }
