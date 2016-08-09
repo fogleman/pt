e4e117a3aab332dce9c750d0a792996a88f91f27
Michael Fogleman <fogleman@gmail.com>
2016-08-09 12:58:10 -0400

    update gopher example

--- examples/gopher.go
+++ examples/gopher.go
@@ -4,18 +4,31 @@ import . "github.com/fogleman/pt/pt"
 
 func main() {
 	scene := Scene{}
-	wall := SpecularMaterial(HexColor(0xFCFAE1), 2)
-	scene.Add(NewSphere(V(4, 7, 3), 2, LightMaterial(Color{1, 1, 1}, 10, NoAttenuation)))
-	scene.Add(NewCube(V(-30, -1, -30), V(-8, 10, 30), wall))
-	scene.Add(NewCube(V(-30, -1, -30), V(30, 0.376662, 30), wall))
-	material := GlossyMaterial(Color{}, 1.5, Radians(30))
-	mesh, err := LoadOBJ("examples/gopher.obj", material)
+
+	// create materials
+	gopher := GlossyMaterial(Color{}, 1.2, Radians(30))
+	wall := GlossyMaterial(HexColor(0xFCFAE1), 1.5, Radians(10))
+	light := LightMaterial(Color{1, 1, 1}, 80, NoAttenuation)
+
+	// add walls and lights
+	scene.Add(NewCube(V(-10, -1, -10), V(-2, 10, 10), wall))
+	scene.Add(NewCube(V(-10, -1, -10), V(10, 0, 10), wall))
+	scene.Add(NewSphere(V(4, 10, 1), 1, light))
+
+	// load and transform gopher mesh
+	mesh, err := LoadOBJ("examples/gopher.obj", gopher)
 	if err != nil {
 		panic(err)
 	}
+	mesh.Transform(Rotate(V(0, 1, 0), Radians(-10)))
 	mesh.SmoothNormals()
+	mesh.FitInside(Box{V(-1, 0, -1), V(1, 2, 1)}, V(0.5, 0, 0.5))
 	scene.Add(mesh)
-	camera := LookAt(V(8, 3, 0.5), V(-1, 2.5, 0.5), V(0, 1, 0), 45)
-	sampler := NewSampler(4, 4)
-	IterativeRender("out%03d.png", 1000, &scene, &camera, sampler, 1920/2, 1080/2, -1)
+
+	// position camera
+	camera := LookAt(V(4, 1, 0), V(0, 0.9, 0), V(0, 1, 0), 40)
+
+	// render the scene
+	sampler := NewSampler(16, 16)
+	IterativeRender("out%03d.png", 1000, &scene, &camera, sampler, 1024, 1024, -1)
 }
