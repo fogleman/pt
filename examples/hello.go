6c9d451e994ef8554e8d2696efa4f6725f59a2d8
Michael Fogleman <fogleman@gmail.com>
2016-08-29 13:53:20 -0400

    hello world example

---
+++ examples/hello.go
@@ -0,0 +1,30 @@
+package main
+
+import . "github.com/fogleman/pt/pt"
+
+func main() {
+	// create a scene
+	scene := Scene{}
+
+	// create a material
+	material := DiffuseMaterial(White)
+
+	// add the floor (a plane)
+	plane := NewPlane(V(0, 0, 0), V(0, 0, 1), material)
+	scene.Add(plane)
+
+	// add the ball (a sphere)
+	sphere := NewSphere(V(0, 0, 1), 1, material)
+	scene.Add(sphere)
+
+	// add a spherical light source
+	light := NewSphere(V(0, 0, 5), 1, LightMaterial(White, 8))
+	scene.Add(light)
+
+	// position the camera
+	camera := LookAt(V(3, 3, 3), V(0, 0, 0.5), V(0, 0, 1), 50)
+
+	// render the scene with progressive refinement
+	sampler := NewSampler(4, 4)
+	IterativeRender("out%03d.png", 1000, &scene, &camera, sampler, 960, 540, -1)
+}
