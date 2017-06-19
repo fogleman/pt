c48dadb27ddfcd75111d8484011dc5719a932500
Michael Fogleman <fogleman@gmail.com>
2017-06-19 15:38:17 -0400

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,33 +1,50 @@
 from fabric.api import *
+from fabric_gce_tools import *
 
-env.user = 'ubuntu'
-env.key_filename = '/Users/fogleman/home.pem'
+# env.user = 'ubuntu'
+# env.key_filename = '/Users/fogleman/home.pem'
 
-env.hosts = [
-    '127.0.0.1',
-]
+# env.hosts = [
+# ]
 
+@parallel
+@roles('pt')
 def init():
-    run('sudo apt-get --assume-yes install git')
-    run('wget https://storage.googleapis.com/golang/go1.6.3.linux-amd64.tar.gz')
-    run('sudo tar -C /usr/local -xzf go1.6.3.linux-amd64.tar.gz')
+    run('sudo apt-get update')
+    # run('sudo apt-get --assume-yes upgrade')
+    run('sudo apt-get --assume-yes install git build-essential')
+    run('wget https://github.com/embree/embree/releases/download/v2.16.2/embree-2.16.2.x86_64.linux.tar.gz')
+    run('tar xzf embree-2.16.2.x86_64.linux.tar.gz')
+    run('rm embree-2.16.2.x86_64.linux.tar.gz')
+    run('echo "source embree-2.16.2.x86_64.linux/embree-vars.sh" >> .profile')
+    run('wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz')
+    run('sudo tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz')
+    run('rm go1.8.3.linux-amd64.tar.gz')
     run('echo "export PATH=$PATH:/usr/local/go/bin" >> .profile')
     run('echo "export GOPATH=$HOME/go" >> .profile')
     run('source .profile')
-    run('go get github.com/fogleman/pt')
-
-def fetch():
-    i = env.hosts.index(env.host)
-    filename = 'fetch%d.tar.gz' % i
+    run('go get -u github.com/fogleman/go-embree')
+    run('go get -u github.com/fogleman/pt/pt')
     with cd('~/go/src/github.com/fogleman/pt'):
-        run('tar czf ~/fetch.tar.gz *.png')
-    get('fetch.tar.gz', filename)
-    local('tar xzf ' + filename)
-    local('rm ' + filename)
+        run('git checkout embree')
 
+@parallel
+@roles('pt')
+def upload(path):
+    put(path)
+
+@parallel
+@roles('pt')
+def start(command):
+    command += ' 2>stderr.txt >stdout.txt &'
+    run(command)
+
+@parallel
+@roles('pt')
 def latest():
     i = env.hosts.index(env.host)
     filename = 'latest%d.png' % i
-    with cd('~/go/src/github.com/fogleman/pt'):
-        run('cp `ls out* | tail -n 2 | head -n 1` ~/latest.png')
+    run('cp `ls out* | tail -n 2 | head -n 1` latest.png')
     get('latest.png', filename)
+
+update_roles_gce(False)
