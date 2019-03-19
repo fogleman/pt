218b4a85683e770fdf09e92208db0e591cd4709d
hborntraeger <pheredhel@gmail.com>
2019-03-19 12:28:45 +0100

    Add orthogonal camera
    Adds a basic orthogonal camera for orthogonal rendering

--- pt/camera.go
+++ pt/camera.go
@@ -1,6 +1,7 @@
 package pt
 
 import (
+	"log"
 	"math"
 	"math/rand"
 )
@@ -46,4 +47,41 @@ func (c *Camera) CastRay(x, y, w, h int, u, v float64, rnd *rand.Rand) Ray {
 		d = focalPoint.Sub(p).Normalize()
 	}
 	return Ray{p, d}
+}
+
+// OrthogonalCamera implements a simple orthogonal camera
+type OrthogonalCamera struct {
+	up, right, pos, dir Vector
+	width               float64
+}
+
+// OrthoLookAt sets up a new camera with the center of the camera at location
+// its center points at the given target (i.e. the target point is centered in the image)
+// up defines the up direction for the camera, pxsize the size of a single pixel
+func OrthoLookAt(location, up, target Vector, width float64) OrthogonalCamera {
+	oc := OrthogonalCamera{
+		up:    up.Normalize(),
+		pos:   location,
+		dir:   target.Sub(location).Normalize(),
+		width: width,
+	}
+	oc.right = oc.dir.Cross(oc.up).Normalize()
+	oc.up = oc.dir.Cross(oc.right).Normalize()
+	log.Printf("%+v", oc)
+	return oc
+}
+
+// CastRay implements the RenderCamera interface and creates the ray used for rendering
+func (oc *OrthogonalCamera) CastRay(x, y, w, h int, u, v float64, rnd *rand.Rand) Ray {
+
+	wf, xf := float64(w), float64(x)
+	hf, yf := float64(h), float64(y)
+	size := oc.width / wf
+	right := oc.right.MulScalar(size * (xf - wf/2 + u))
+	up := oc.up.MulScalar(size * (yf - hf/2 + v))
+
+	return Ray{
+		Origin:    oc.pos.Add(up).Add(right),
+		Direction: oc.dir,
+	}
 }
